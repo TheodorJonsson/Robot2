0e9f7046374fcd8fd8f7fe5a0308ba7865d1df2e
Theodor Jonsson <ens18trn@cs.umu.se>
2022-04-19 13:37:07 +0200

    Sent in files to git

---
+++ Maze.java
@@ -0,0 +1,200 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Author: Theodor Jonsson, ens18trn
+ * Date: April 13 2022
+ * Course: Objektorienterad Programmeringsmetodik
+ */
+
+/**
+ * Class name: Maze
+ * The class reads a text file from a scanner to a maze. RandomRobot uses this class to
+ * check if position is valid or not. The robot also uses this class to get the start
+ * and check if it has reached goal or not
+ */
+
+public class Maze
+{
+    private ArrayList<String> maze = new ArrayList<>();
+    private int rows;
+    /**
+     * Constructor: Maze
+     * Reads a from a scanner and puts the lines in to a arraylist of strings
+     *
+     * @params scanner, which is the scanned file from the argument
+     * @throws Exception, checks if the scanner is empty or not
+     */
+    public Maze(Scanner scanner) throws Exception
+    {
+        int amount_of_lines = 0;
+        while(scanner.hasNextLine())
+        {
+            this.maze.add(scanner.nextLine());
+            amount_of_lines++;
+        }
+        this.rows = maze.size();
+
+        if(amount_of_lines == 0)
+        {
+            throw new Exception("Scanner is empty");
+        }
+
+        isMazeValid();
+    }
+
+    /**
+     * Method: isMazeValid
+     * Checks if the maze has any non valid characters in it
+     */
+    private void isMazeValid()
+    {
+        for(int i = 0; i < this.rows; i++)
+        {
+            String rowIndex = maze.get(i);
+            {
+                for(int j = 0;j < rowIndex.length(); j++)
+                {
+                    char character = rowIndex.charAt(j);
+                    if((character != ' ') && (character != '*') && (character != 'S') && (character != 'G'))
+                    {
+                        throw new IllegalArgumentException("Non valid character is in the maze");
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Method: isMovable
+     * Firstly checks if the position is in bounds of the maze.
+     * Then checks if the position is a goal or a space
+     *
+     * @param p, which is the position to be checked
+     * @return true if the position is a goal or a space, false if the position is out of bounds
+     * or a non valid character
+     */
+    public boolean isMovable(Position p)
+    {
+        int cols = this.getNumColumns();
+        int rows = this.getNumRows();
+
+        if(((p.getX() < 0) || (p.getX() > (cols - 1)) || (p.getY() <= 0) || p.getY() > (rows - 1)))
+        {
+            return false;
+        }
+
+        String rowIndex = maze.get(p.getY());
+        if((rowIndex.charAt(p.getX()) != '*'))// || (rowIndex.charAt(p.getX()) == 'G'))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Method: isGoal
+     * Checks if the position is a goal
+     *
+     * @param p, which is the position to be checked
+     * @return true if the position is a goal, false if its anything else
+     */
+    public boolean isGoal(Position p)
+    {
+        String rowIndex = maze.get(p.getY());
+        if(rowIndex.charAt(p.getX()) == 'G')
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Method: getStart
+     * Searches for the position of the start if it can't find it
+     * throws a illegal argument.
+     * @return startPos, which is the position of the start if it can find it.
+     * @throws IllegalArgumentException
+     */
+    public Position getStart() throws IllegalArgumentException
+    {
+
+        Position startPos = new Position();
+        for(int i = 0; i < this.rows; i++)
+        {
+            String rowIndex = maze.get(i);
+            int colIndex = rowIndex.indexOf('S');
+            if(colIndex != -1)
+            {
+                startPos = new Position(colIndex, i);
+                return startPos;
+            }
+        }
+
+        if (startPos.getX() == -1 && startPos.getY() == -1) {
+            throw new IllegalArgumentException("Could not find Start.");
+        }
+        return startPos;
+
+    }
+
+    /**
+     * Method: getNumColumns
+     * Searches through the maze to get the largest column in the mze
+     * @return maxLength, which is the largest column
+     */
+    public int getNumColumns()
+    {
+        int maxLength = 0;
+        for(int i = 0; i < this.rows; i++)
+        {
+
+            if(maxLength < maze.get(i).length())
+            {
+                maxLength = maze.get(i).length();
+            }
+        }
+        return maxLength;
+
+    }
+
+    /**
+     * Method: getNumRows
+     *
+     * @return this.rows, which is the amount of rows in the maze
+     */
+    public int getNumRows()
+    {
+        return this.rows;
+    }
+    /*
+     * ----NOT USED CODE----
+     * Method used for checking where robot is and changing the corresponding string
+     * to show a 'o' to show where the robot is and then change the string back to its original value
+     */
+    public void printMaze(Position playerPos)
+    {
+        for(int i = 0; i < this.rows; i++)
+        {
+            if(playerPos.getY() == i)
+            {
+                String changeString = maze.get(i);
+                String temp = changeString;
+                char ch = '+';
+                changeString = changeString.substring(0, playerPos.getX()) + ch + changeString.substring(playerPos.getX() + 1);
+                maze.set(i, changeString);
+                System.out.println(maze.get(i));
+                maze.set(i, temp);
+
+            }
+            else
+            {
+                System.out.println(maze.get(i));
+            }
+        }
+
+    }
+        /**/
+
+}
---
+++ Position.java
@@ -0,0 +1,136 @@
+/**
+ * Author: Theodor Jonsson, ens18trn
+ * Date: April 13 2022
+ * Course: Objektorienterad Programmeringsmetodik
+ */
+
+/**
+ * Class: Position
+ * Creates a position which has a x and a y value.
+ * Can get the positions around it but not diagonally aswell as
+ * checking if two positions are equal using a hash.
+ */
+public class Position
+{
+    private int x;
+    private int y;
+
+    /**
+     * Constructor: Position
+     * Default values for a x and y
+     */
+    public Position()
+    {
+        this.x = -1;
+        this.y = -1;
+    }
+
+    /**
+     * Constructor: Positio
+     * Creates a position with x and y values from paramaters
+     *
+     * @param x
+     * @param y
+     */
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Method: getX
+     * @return x, value of current object.
+     */
+    public int getX()
+    {
+        return this.x;
+    }
+    /**
+     * Method: getY
+     * @return Y, value of current object.
+     */
+    public int getY()
+    {
+        return this.y;
+    }
+
+    /**
+     * Method: getPosToSouth
+     * Gets the position south of the current position.
+     * @return pos, which is newly created position
+     */
+    public Position getPosToSouth()
+    {
+        Position pos = new Position(x, (y + 1));
+        return pos;
+    }
+
+    /**
+     * Method: getPosToNorth
+     * Gets the position north of the current position.
+     * @return pos, which is newly created position
+     */
+    public Position getPosToNorth()
+    {
+        Position pos = new Position(x, (y - 1));
+        return pos;
+    }
+
+    /**
+     * Method: getPosToWest
+     * Gets the position west of the current position.
+     * @return pos, which is newly created position
+     */
+    public Position getPosToWest()
+    {
+        Position pos = new Position((this.x - 1), this.y);
+        return pos;
+    }
+
+    /**
+     * Method: getPosToEast
+     * Gets the position east of the current position.
+     * @return pos, which is newly created position
+     */
+    public Position getPosToEast()
+    {
+        Position pos = new Position((this.x + 1), this.y);
+        return pos;
+    }
+
+    /**
+     * Method: equals
+     * Checks if the object o is a position then checks if
+     * the object o has the same hashcode as the current position
+     * @param o
+     * @return true if they have the same hashcode, false if not
+     */
+    @Override
+    public boolean equals(Object o)
+    {
+        if(o instanceof Position)
+        {
+            Position test = (Position) o;
+            if(this.hashCode() == test.hashCode())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Method: hashCode
+     * Creates a hash value using the x and y position
+     * @return hash, which is the hashed value
+     */
+    public int hashCode()
+    {
+        int hash = 7;
+        int temp = (this.getY() + ((this.getX() + 1)/2));
+
+        hash = 31 * hash * temp;
+        return hash;
+    }
+}
