d4495f752b68f43faaa952bd340bcbab25f9259c
Theodor Jonsson <ens18trn@cs.umu.se>
2022-04-26 13:38:11 +0200

    Fixed bug in righthand

--- RightHandRuleRobot.java
+++ RightHandRuleRobot.java
@@ -71,9 +71,6 @@ public class RightHandRuleRobot implements Robot {
             previousPosition = p;
         }
         setDirections();
-
-
-        this.maze.printMaze(this.getPosition());
     }
 
 
